d51a663d483cade5ac82abe5d96681724cdb0d6b
BenedikteEva <cph-bm109@cphbusiness.dk>
2018-03-13 12:37:36 +0100

    discriminatorvalues

--- src/main/java/entity/CityInfo.java
+++ src/main/java/entity/CityInfo.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package entity;
 
 import java.io.Serializable;
@@ -11,10 +7,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-/**
- *
- * @author Ejer
- */
 @Entity
 public class CityInfo extends Address implements Serializable {
 
