2937833d025c48ec338aa3089e4834317a19e4b4
BenedikteEva <cph-bm109@cphbusiness.dk>
2018-03-14 12:04:19 +0100

    Merge origin/Developer into DataGenerator
    Conflicts:
    	src/main/java/facade/PersonFacade.java
    	src/main/resources/META-INF/persistence.xml

--- src/main/java/persistence/SchemaBuilder.java
+++ src/main/java/persistence/SchemaBuilder.java
@@ -15,14 +15,14 @@ import javax.persistence.Persistence;
 public class SchemaBuilder {
 
     public static void main(String[] args) {
-        
+
         HashMap puproperties = new HashMap();
         puproperties.put("javax.persistence.sql-load-script-source", "scripts/cityinfoscript.sql");
-        
+
 // puproperties.put("javax.persistence.sql-load-script-source" ,"oneTimeSetupCa2.sql");
         Persistence.generateSchema("CourseAssignment2_war_1.0-SNAPSHOTPU", puproperties);
-        
-        puproperties.remove( "scripts/cityinfoscript.sql");
+
+        puproperties.remove("scripts/cityinfoscript.sql");
 //Persistence.generateSchema("CourseAssignment2_war_1.0-SNAPSHOTPU", null);
     }
 }
