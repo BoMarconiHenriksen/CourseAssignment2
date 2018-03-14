c51de7f0a77d95166dd0dd9f4d5aaa03f410f82a
Ticondrus <Ticondrus@10.50.137.199>
2018-03-14 12:50:46 +0100

    Get person by hobby, address, name, phone er lavet. Men mangler stavefejl for at det virker.

--- src/main/java/facade/HobbyFacade.java
+++ src/main/java/facade/HobbyFacade.java
@@ -1,5 +1,34 @@
 package facade;
 
+import entity.Person;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
 public class HobbyFacade {
+    
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("CourseAssignment2_war_1.0-SNAPSHOTPU");
+    EntityManager em = emf.createEntityManager();
+
+    public List<Person> getPersonbyHobby(String description) {
+
+        Query q = em.createQuery("SELECT c FROM Hobby c where c.description =" + description);
+        q.setParameter("description", description);
+        List<Person> persons = q.getResultList();
+        return persons;
+
+    }
+    
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("CourseAssignment2_war_1.0-SNAPSHOTPU");
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        HobbyFacade pf = new HobbyFacade();
+        System.out.println(pf.getPersonbyHobby("Brian"));
+        em.close();
+    }
     
 }
