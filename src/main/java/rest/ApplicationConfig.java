2ffe898ff214cf259abf75c436080444c7aa79d4
Ticondrus <Ticondrus@classic-elite14.home>
2018-03-17 19:15:24 +0100

    Javadoc på ApplicationConfig.java og PersonsResource.java

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -7,6 +7,14 @@ import javax.ws.rs.core.Application;
 
 @javax.ws.rs.ApplicationPath("api")
 public class ApplicationConfig extends Application {
+    
+    /**
+     * ApplicationConfig is the main java class that represents all resources api's.
+     * You run it by naviagting to the application path in yo url.
+     * You can further run the given resource by navigating further to the given resource.
+     *
+     * @return resources
+     */
 
     @Override
     public Set<Class<?>> getClasses() {
